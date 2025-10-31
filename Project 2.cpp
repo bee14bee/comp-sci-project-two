8d63e63377c956f9b19a16452cbfa4d1263300c5
bee14bee <brit.rod1005@gmail.com>
2025-10-30 21:06:24 -0500

    Fix syntax errors and enhance trap placement logic
    Fixed syntax errors and improved random placement logic.

--- Project 2.cpp
+++ Project 2.cpp
@@ -3,16 +3,60 @@ Code for project two
 */
 
 #include <iostream>
+#include <cstdlib> //for rand gen
+#include <ctime> //for rand gen
 using namespace std;
+
+//Void function for making the visible and hidden array 
+void initializeBoards(int hidden[ROWS][COLS, char visible[ROWS][COLS])
+{
+	for(int r=0;r<ROWS;r++)
+	{
+		for(int c=0;c<COLS;c++)
+	      {
+	        hiddenBoard[r][c]=0;
+	        visibleBoard[r][c]='*';
+	      }
+	  }
+}
+
+int placingTraps
+
 
 int main(){
 
 
+	//Randomly place Treasure and traps on hiddenBoard 
+
+	srand(time(0));
+	int trap=1;
+	for(int r=0;r<ROWS;r++)
+	{
+		for(int c=0;c<COLS;c++)
+	      {
+					while(
+					cout<<"Pick random Row: "<<r<<" and random column: "<<c;
+					r=rand()%5+0;
+					c=rand()%5+0;
+	        
+	
+	        
+	      }
+	  }
 
 
 
 
-
+	
+	//displaying visible array
+	for (int r=0;r<ROWS;r++)
+	{
+		for(int c=0;c<COLS;c++)
+	      {
+	        cout<< visibleBoard[r][c] <<" ";
+	      }
+	    cout<<endl;
+	  } 
 
 
 return 0;
